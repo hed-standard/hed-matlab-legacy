22908848e88acb6b16b65e26084967fe4d057594
Dung Truong <dt.young112@gmail.com>
2019-07-26 15:16:35 -0700

    CSRF protect with POST

--- matlab/helpers/validation/validateHedStrings.m
+++ matlab/helpers/validation/validateHedStrings.m
@@ -1,11 +1,11 @@
 function issues = validateHedStrings(hedxml, hedStrings, generateWarnings)
 % set up server destination
-host = 'http://localhost:33000';
-url = [host '/eegvalidation'];
+host = 'http://localhost:5000';
+url = [host '/eegsubmit'];
 
-% retrieve CSRF token
-%  spreadsheetform = urlread([host '/validation']);
-%  csrftoken = getCSRFToken(spreadsheetform);
+% retrieve CSRF token and cookies
+[~,response] = system(['curl -v ' host '/eegvalidation']);
+[csrftoken, cookie] = getCSRFCookies(response);
 
 % prepare data
 if isempty(hedxml)
@@ -18,7 +18,7 @@ end
 % submit request to server for validation results
 try
     fprintf(['Validating through server at ' url ' ...\n']);
-    command = sprintf('curl -X GET -F hed_xml_file=%s -F hed_strings=''{"hed_strings":%s};type=application/json'' -F check_for_warnings=%d %s', hed_xml_file, jsonencode(hedStrings), generateWarnings, url);
+    command = sprintf('curl -X POST -H "X-CSRFToken: %s" -H "Cookie: %s" -F hed_xml_file=%s -F hed_strings=''{"hed_strings":%s};type=application/json'' -F check_for_warnings=%d %s', csrftoken, cookie, hed_xml_file, jsonencode(hedStrings), generateWarnings, url);
     [status, result] = system(command);
 catch
     ME = MException('validateHedString:serverError', ...
@@ -38,11 +38,16 @@ else
         'Failed to validate hed strings');
     throw(ME);
 end
+    % Get CSRF token and session cookies from server response
+    function [csrftoken, cookie] = getCSRFCookies(str)
+        csrfIdx = strfind(str,"csrf_token");
+        tmp = str(csrfIdx(1)+strlength("csrf_token")+1:end);
+        csrftoken = regexp(tmp,'".*?"','match');
+        csrftoken = csrftoken{1}(2:end-1);
 
-%     function csrftoken = getCSRFToken(str)
-%         csrfIdx = strfind(str,"csrf_token");
-%         tmp = str(csrfIdx(1)+strlength("csrf_token")+1:end);
-%         csrftoken = regexp(tmp,'".*?"','match');
-%         csrftoken = csrftoken{1}(2:end-1);
-%     end
+        cookieIdx = strfind(str,"Set-Cookie");
+        tmp = str(cookieIdx(1)+strlength("Set-Cookie")+2:end);
+        cookie = regexp(tmp,'session.*?;','match');
+        cookie = cookie{1}(1:end-1);
+    end
 end
